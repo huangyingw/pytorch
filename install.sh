b913df28f2e7a0771389ce837552a6bd54b733c9
Ying Huang <huangyingw@gmail.com>
2020-08-22 11:00:16 -0700

    n

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ conda install numpy ninja pyyaml mkl mkl-include setuptools cmake cffi typing_ex
 # Add LAPACK support for the GPU if needed
 conda install -c pytorch magma-cuda102  # or [ magma-cuda101 | magma-cuda100 | magma-cuda92 ] depending on your cuda version
 
+git submodule init
 git submodule sync
 git submodule update --init --recursive
 
